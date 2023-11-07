4037c089983b452a3cbfeb1ad0ac68240586f690
David <davidtran31.31@gmail.com>
2023-11-07 09:36:14 +0100

    Initialisation du projet

---
+++ main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.widget import Widget 
+from kivy.uix.relativelayout import RelativeLayout
+
+
+
+
+class MainWidget(RelativeLayout):
+    pass
+
+
+class MrBeat(App):
+    pass
+
+
+
+MrBeat().run()
+
+
